13bc1b2eeacd1f39db8ce311d7cf1f302cdf72b9
Hana Kysela <hana.kysela@gmail.com>
2016-04-08 16:10:58 +0200

    initial tree for Titanic

--- Titanic/KaggleTitanic.R
+++ Titanic/KaggleTitanic.R
@@ -1,19 +1,41 @@
-setwd("C:/Users/Weka_Hana/Dropbox/R/Kaggle/Titanic")
+#2016_04_08
 
-train<-read.csv("train.csv")
+# https://www.kaggle.com/c/titanic SOURCE
 
-train.data.file <- "train.csv"
+# tutorials:
 
-train.column.types <- c('integer',   # PassengerId
-                        'factor',    # Survived 
-                        'factor',    # Pclass
-                        'character', # Name
-                        'factor',    # Sex
-                        'numeric',   # Age
-                        'integer',   # SibSp
-                        'integer',   # Parch
-                        'character', # Ticket
-                        'numeric',   # Fare
-                        'character', # Cabin
-                        'factor'     # Embarked
-)
+      # http://trevorstephens.com/post/72916401642/titanic-getting-started-with-r
+      # https://github.com/wehrley/wehrley.github.io/blob/master/SOUPTONUTS.md
+
+#### 1. packages ####
+install.packages("rpart")
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+
+  
+#### 2. Organize everything ####
+  setwd("~/Git/Kaggle/Titanic")
+
+  train<-read.csv("train.csv")
+
+  train$Pclass <-as.factor(train$Pclass)
+  
+#### 3. data viz ####♥ 
+  fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
+  
+  fancyRpartPlot(fit) # nice tree
+  
+  # What about N/A's??
+  
+  
+# very first submission
+  Prediction <- predict(fit, test, type = "class")
+  submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+  write.csv(submit, file = "myfirstdtree.csv", row.names = FALSE)
