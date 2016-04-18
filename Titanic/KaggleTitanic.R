7c4730584aa184df3e23a68ede247dd67f7e8fae
Hana Kysela <hana.kysela@gmail.com>
2016-04-18 14:02:50 +0200

    check for Age->Child status, still not working

--- Titanic/KaggleTitanic.R
+++ Titanic/KaggleTitanic.R
@@ -71,6 +71,7 @@
       test$Child <-as.factor(test$Child)
     
   
+      
 #### MODEL 1 - Age grouped into Child (with 3 levels), assumes missing age=adult ####
       fit_01 <- rpart(Survived ~ Pclass + Sex + Age + SibSp, data=train, method="class")
       fit_02 <- rpart(Survived ~ Pclass + Sex, data=train, method="class")
@@ -84,7 +85,8 @@
       fit_10 <- rpart(Survived ~ Pclass + Sex + Age + Fare + SibSp + Parch, data=train, method="class")
       
   
-#### MODEL 2 - gess age (resp. Child status):
+      
+#### MODEL 2 - guess age (resp. Child status): ####
   
       train$Child <- "NA"
       train$Child[train$Age < 81] <- "Adult"
@@ -115,42 +117,45 @@
       summary(test_01$Child)
       summary(train$Child)
   
+      
+      
+      
+#### MODEL 2 - GUESSING THE AGE OF PERSON ####      
+      CHfit_01<- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = train, method = "class")
+      # show that everyone is Adult, but when SibSp >2,5, they are Children
+      CHfit_02<- rpart(Child ~ SibSp + Parch, data = train, method = "class")
+      # confirms CHfit_01
+      CHfit_03<- rpart(Child ~ Pclass + Sex + Fare + Parch, data = train, method = "class")
+      # removing SibSp from the model makes it very complicated -> likely to overfit
+      CHfit_04 <- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = combined, method = "class")
+      # very complicated -> overfitting
+      CHfit_05 <- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = combined_01, method = "class")
+      # if Parch>0.5 & SibSp > 3.5 ->children
+      CHfit_06 <- rpart(Child ~ Pclass +SibSp + Parch, data = combined_01, method = "class")
+      # viz CHfit_05
+      CHfit_07 <- rpart(Child ~ SibSp, data = combined_01, method = "class")
+      # All adults except when SibSp>3,5, then Children
+      
  
   train_knowage<-train[complete.cases(train[ ,5:6]),] #I know age
   train_dontknowage <- train[(train$Child)== "NA", ] # NA in Child column
   train_dontknowage$Child[train_dontknowage$SibSp <4] <- "Adult"
   train_dontknowage$Child[train_dontknowage$SibSp >3] <- "Children"
-  train_03<-rbind(train_knowage, train_dontknowage)
+  train_age<-rbind(train_knowage, train_dontknowage)
   
   
   test_knowage<-test[complete.cases(test[ ,5:6]),] #I know age
   test_dontknowage <- test[(test$Child)== "NA", ] # NA in Child column
   test_dontknowage$Child[test_dontknowage$SibSp <4] <- "Adult"
   test_dontknowage$Child[test_dontknowage$SibSp >3] <- "Children"
-  test_03<-rbind(test_knowage, test_dontknowage)
+  test_age<-rbind(test_knowage, test_dontknowage)
   
   
-#---------nahore je ta myslenka - preklasifikovat trees podle tohoto noveho train_03 datasetu.
+#---------nahore je ta myslenka - preklasifikovat trees podle tohoto noveho train_age datasetu.
   # Child je doplneno, tak by se mohl tree lepe ucit.
   
-
-  CHfit_01<- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = train, method = "class")
-   # show that everyone is Adult, but when SibSp >2,5, they are Children
-  CHfit_02<- rpart(Child ~ SibSp + Parch, data = train, method = "class")
-   # confirms CHfit_01
-  CHfit_03<- rpart(Child ~ Pclass + Sex + Fare + Parch, data = train, method = "class")
-   # removing SibSp from the model makes it very complicated -> likely to overfit
-  CHfit_04 <- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = combined, method = "class")
-   # very complicated -> overfitting
-  CHfit_05 <- rpart(Child ~ Pclass + Sex + Fare +SibSp + Parch, data = combined_01, method = "class")
-   # if Parch>0.5 & SibSp > 3.5 ->children
-  CHfit_06 <- rpart(Child ~ Pclass +SibSp + Parch, data = combined_01, method = "class")
-   # viz CHfit_05
-  CHfit_07 <- rpart(Child ~ SibSp, data = combined_01, method = "class")
-   # All adults except when SibSp>3,5, then Children
   
-  
- # Child-Age adjusted model
+ # Child-Age adjusted model - NOT WORKING
   ACH_fit_01 <- rpart(Survived ~ Pclass + Sex + Age + SibSp, data=train_03, method="class")
   ACH_fit_02 <- rpart(Survived ~ Pclass + Sex, data=train_03, method="class")
   # ACH_fit_03 <- rpart(Survived ~ Pclass + Sex + Age, data=train_03, method="class")
@@ -163,7 +168,23 @@
   ACH_fit_10 <- rpart(Survived ~ Pclass + Sex + Age + Fare + SibSp + Parch, data=train_03, method="class")
   
   
-#### Plotting trees ####
+  
+#### MODEL 2 - FIT TREES
+  fit_01 <- rpart(Survived ~ Pclass + Sex + Age + SibSp, data=train, method="class")
+  fit_02 <- rpart(Survived ~ Pclass + Sex, data=train, method="class")
+  fit_03 <- rpart(Survived ~ Pclass + Sex + Age, data=train, method="class")
+  fit_04 <- rpart(Survived ~ Pclass + Sex + Child, data=train, method="class")
+  fit_05 <- rpart(Survived ~ Pclass + Sex + Child + SibSp, data=train, method="class")
+  fit_06 <- rpart(Survived ~ Pclass + Sex + Child + Fare, data=train, method="class")
+  fit_07 <- rpart(Survived ~ Pclass + Sex + Child + Fare + SibSp + Parch, data=train, method="class")
+  fit_08 <- rpart(Survived ~ Pclass + Sex + Fare, data=train, method="class")
+  fit_09 <- rpart(Survived ~ Pclass + Sex + Fare + Age, data=train, method="class")
+  fit_10 <- rpart(Survived ~ Pclass + Sex + Age + Fare + SibSp + Parch, data=train, method="class")
+  
+  
+  
+  
+#### Plotting trees - OLD MODELS####
   fancyRpartPlot(fit_01) # nice tree
   fancyRpartPlot(fit_02)
   fancyRpartPlot(fit_03)
@@ -181,6 +202,9 @@
   fancyRpartPlot(CHfit_05)
   fancyRpartPlot(CHfit_06)
   fancyRpartPlot(CHfit_07)
+  
+  
+#### PLOTTING THE TREES NEW MODELS ####  
   fancyRpartPlot(ACH_fit_01)
   fancyRpartPlot(ACH_fit_02)
   fancyRpartPlot(ACH_fit_03)
@@ -194,8 +218,19 @@
   
   
   
+  age_fit_01 <- rpart(Survived ~ Pclass + Sex + Age + SibSp, data=train_age, method="class")
+  age_fit_02 <- rpart(Survived ~ Pclass + Sex, data=train_age, method="class")
+  age_fit_03 <- rpart(Survived ~ Pclass + Sex + Age, data=train_age, method="class")
+  age_fit_04 <- rpart(Survived ~ Pclass + Sex + Child, data=train_age, method="class")
+  age_fit_05 <- rpart(Survived ~ Pclass + Sex + Child + SibSp, data=train_age, method="class")
+  age_fit_06 <- rpart(Survived ~ Pclass + Sex + Child + Fare, data=train_age, method="class")
+  age_fit_07 <- rpart(Survived ~ Pclass + Sex + Child + Fare + SibSp + Parch, data=train_age, method="class")
+  age_fit_08 <- rpart(Survived ~ Pclass + Sex + Fare, data=train_age, method="class")
+  age_fit_09 <- rpart(Survived ~ Pclass + Sex + Fare + Age, data=train_age, method="class")
+  age_fit_10 <- rpart(Survived ~ Pclass + Sex + Age + Fare + SibSp + Parch, data=train_age, method="class")
+  
 #### KAGGLE SCORES ####
-  # Prediction_02(Survived ~ Pclass + Sex)    ->                              0.76555 
+  # Prediction_02(Survived ~ Pclass + Sex)    ->                              0.76555
   # Prediction_08(Survived ~ Pclass + Sex + Fare) ->                          0.78947 ## (should have been 0,77990)
   # Prediction_03(Survived ~ Pclass + Sex + Age ->                            0.73684
   # Prediction_04(Survived ~ Pclass + Sex + Child) ->                         0.75598
@@ -210,10 +245,25 @@
   # ACH_Prediction_07(Survived ~ Pclass + Sex + Child + Fare + SibSp + Parch) 0.55981
   # ACH_Prediction_10(Survived ~ Pclass + Sex + Age + Fare + SibSp + Parch) ->0.56459  
   # ACH_Prediction_02(Survived ~ Pclass + Sex)    ->                          0.58373
+  
+  # Age_Prediction_02<- Prediction_02(Survived ~ Pclass + Sex) ->             0.58373
 
-#NECO JE SPATNE, PO ODLADENI NE CHILD GROUP VYCHAZI HUR I MODEL ZALOZENY NA PCLASS A SEX
+#NECO JE SPATNE, PO ODLADENI NA CHILD GROUP VYCHAZI HUR I MODEL ZALOZENY NA PCLASS A SEX
   
 #### write to the submission file ####
-  ACH_Prediction_02<- predict(ACH_fit_02, test, type = "class")
-  submit <- data.frame(PassengerId = test_03$PassengerId, Survived = ACH_Prediction_02)
-  write.csv(submit, file = "ACH_Prediction_02.csv", row.names = FALSE)
+  prediction_x2<- predict(fit_02, test, type = "class")
+  submit <- data.frame(PassengerId = test$PassengerId, Survived = prediction_x2)
+  write.csv(submit, file = "prediction_x2.csv", row.names = FALSE)
+
+  
+#### COMMENTS ####
+prediction_10 <- read.csv("prediction_10.csv")
+Age_Prediction_02 <- read.csv("Age_Prediction_02.csv")
+
+prediction_10$Survived<-as.factor(prediction_10$Survived)
+Age_Prediction_02$Survived <- as.factor(Age_Prediction_02$Survived)
+summary(prediction_10)
+summary(Age_Prediction_02)
+
+summary(prediction_10$Survived)
+summary(Age_Prediction_02$Survived)
